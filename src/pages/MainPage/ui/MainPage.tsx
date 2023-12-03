028aaf2ee2bf86de754c1841b9cbdf4cc72168e8
RevinFedor <fr75793@gmail.com>
2023-12-04 01:05:15 +0500

    123

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -4,7 +4,6 @@ import { Input } from '@/shared/ui/Input';
 import { Text } from '@/shared/ui/Text';
 import { Page } from '@/widgets/Page';
 import { Listbox } from '@/shared/ui/Popups';
-import { RatingCard } from '@/entities/Rating';
 
 const MainPage = () => {
   const [value, setValue] = useState('input the');
@@ -13,11 +12,7 @@ const MainPage = () => {
       <Text />
       <Input placeholder="The place" value={value} onChange={setValue} />
       <Listbox />
-      <RatingCard
-        hasHasmodal
-        title="Как вам статья?"
-        modalTitle="Оставьте свой отзыв"
-      />
+     
     </Page>
   );
 };
