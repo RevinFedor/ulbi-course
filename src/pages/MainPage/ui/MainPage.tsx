6e03edd50104de4e46bc30aa9de07827e965f64d
RevinFedor <fr75793@gmail.com>
2023-12-04 05:29:05 +0500

    Сохранение текущих изменений

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -4,6 +4,7 @@ import { Input } from '@/shared/ui/Input';
 import { Text } from '@/shared/ui/Text';
 import { Page } from '@/widgets/Page';
 import { Listbox } from '@/shared/ui/Popups';
+import { RatingCard } from '@/entities/Rating';
 
 const MainPage = () => {
   const [value, setValue] = useState('input the');
